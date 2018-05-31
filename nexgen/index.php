fc3ccc8159aae428d0a206f0ad34d548b78062f2
Arun Dhungel <arun.dhungel2@gmail.com>
2018-05-31 12:18:36 +0545

    Add files via upload

---
+++ nexgen/index.php
@@ -0,0 +1,618 @@
+
+<!DOCTYPE html>
+<html>
+
+<!-- Head -->
+<head>
+
+	<title>NexGen</title>
+
+	<!-- Meta-Tags -->
+		
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Felt</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!-- //Meta-Tags -->
+		
+		<link rel="icon" href="images/favicon.png">
+	
+		<link rel="stylesheet" href="css/bootstrap.min.css" 	type="text/css" media="all">
+		<link rel="stylesheet" href="css/style.css" 			type="text/css" media="all">
+		<link rel="stylesheet" href="css/chocolat.css" 		type="text/css" media="all">
+		<link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" 	type="text/css">
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:400,700" 				type="text/css">
+		<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+		<link href="fontawesome/css/font-awesome.min.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/reset.css">
+
+
+        <!-- slider logo -->
+
+			<link href="logoslider/css/style.css" rel="stylesheet" type="text/css" />
+			<script type="text/javascript" src="logoslider/js/jquery.flexisel.js"></script>
+		<!-- //slider logo -->
+
+</head>
+<!-- //Head -->
+
+
+
+<!-- Body -->
+<body>
+	<!-- Header -->
+	<div class="header" id="home">
+
+		
+		<!-- Navbar -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container clearfix">
+			<div class="logo">
+				<a href="#"><img src="images/logo.png" alt="Logo"></a>
+			</div>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<!-- Navbar-Collapse -->
+				<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--francisco clearfix">
+						
+						<ul class="nav navbar-nav menu__list">
+						<li class="menu__item"><a href="#" class="menu__link"><span class="menu__helper">HOME</span></a></li>
+							<li class="menu__item"><a href="#about" class="menu__link"><span class="menu__helper">ABOUT</span></a></li>
+							
+							<li class="menu__item"><a href="#services" 	class="menu__link"><span class="menu__helper">SERVICES </span></a></li>
+							<li class="menu__item"><a href="#clients" class="menu__link"><span class="menu__helper">CLIENTS</span></a></li>
+							<li class="menu__item"><a href="#partners" class="menu__link"><span class="menu__helper">ALLIANCES  </span></a></li>
+							<li class="menu__item"><a href="#contact" class="menu__link"><span class="menu__helper">CONTACT  </span></a></li>
+						</ul>
+					</nav>
+				</div>
+				<!-- //Navbar-Collapse -->
+			</div>
+
+		</nav>
+		<!-- //Navbar -->
+
+
+		<!-- Slider -->
+		<div class="slider clearfix">
+			<div class="callbacks_container">
+
+				<ul class="rslides" id="slider">
+
+					<li>
+						<div class="slider-img">
+							<img src="images/slide-5.jpg" class="img-responsive" alt="Manufactory">
+						</div>
+						
+					</li>
+
+					<li>
+						<div class="slider-img">
+							<img src="images/slide-5.jpg" class="img-responsive" alt="Manufactory">
+						</div>
+					</li>
+
+					<li>
+						<div class="slider-img">
+							<img src="images/slide-5.jpg" class="img-responsive" alt="Manufactory">
+						</div>
+					</li>
+
+					<li>
+						<div class="slider-img">
+							<img src="images/slide-5.jpg" class="img-responsive" alt="Manufactory">
+						</div>
+						
+					</li>
+
+					<li>
+						<div class="slider-img">
+							<img src="images/slide-5.jpg" class="img-responsive" alt="Manufactory">
+						</div>
+						
+					</li>
+
+				</ul>
+
+			</div>
+		</div>
+		<!-- //Slider -->
+
+	</div>
+
+	<!-- //Header -->
+
+
+	<!-- About -->
+	<div class="about" id="about">
+		<div class="container">
+
+			<h1><a href="#">ABOUT<i class="fa fa-opencart" aria-hidden="true"></i></a></h1>
+				<p>NexGen IT Solution Pvt. LTD is a firm based in Kathmandu that is committed to provide End-to End IT solutions, with an aim to contribute to the IT infrastructure requirements of companies of all sizes in Nepal. We lay the ground work for electronic highways required for successful Information technology implementations and provide end to end hardware solutions to companies of all sizes. </p>
+				<p>Everyday, we use our industry insight, technological vision and innovative thinking to produce fresh perspectives on our customers’ businesses. That’s how we enable them to reach their full potential and accelerate their business. We are excited to work in the intersection of technology and business. Our game-changing IT innovations and services help our customers in the public and private sectors in Nepal to realise new business and service opportunities, master industry change and stay one step ahead of their competitors. NexGen is headquartered in Kathmandu, Nepal. We are the leading IT services company, but we also have a strong presence in Nepal and delivery capabilities. </p>
+
+				<div class="underline1"></div>
+
+			<!-- Explore-Button --><!-- 
+				<a class="btn" href="#portfolio">EXPLORE</a> -->
+			<!-- //Explore-Button -->
+		</div>
+	</div>
+	<!-- //About -->
+
+	<!-- Services -->
+	<div class="services" id="services">
+		<div class="container">
+
+			<h2><a href="#">SERVICES</a></h2>
+			
+			<div class="underline1"></div>
+
+			<div class="service-grids clearfix">
+				<div class="col-md-2 col-sm-3 service-grid service1">
+					<div class="row services-item">
+						<img src="images/service-icon-1.png" alt="Manufactory">
+						<div class="services-info">
+							<h4><a href="#">Complete Networking Solutions</a></h4>
+							<div class="underline2"></div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 col-sm-3 service-grid service2">
+					<div class="row services-item">
+						<img src="images/service-icon-2.png" alt="Manufactory">
+						<div class="services-info">
+							<h4><a href="#">Network/IT Security</a></h4>
+							<div class="underline2"></div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 col-sm-3 service-grid service3">
+					<div class="row services-item">
+						<img src="images/service-icon-3.png" alt="Manufactory">
+						<div class="services-info">
+							<h4><a href="#">Secure Wireless Solutions</a></h4>
+							<div class="underline2"></div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 col-sm-3 service-grid service4">
+					<div class="row services-item">
+						<img src="images/service-icon-4.png" alt="Manufactory">
+						<div class="services-info">
+							<h4><a href="#">Website Design & Development</a></h4>
+							<div class="underline2"></div>
+							
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-2 col-sm-3 service-grid service5">
+					<div class="row services-item">
+						<img src="images/service-icon-5.png" alt="Manufactory">
+						<div class="services-info">
+							<h4><a href="#">Remote Support</a></h4>
+							<div class="underline2"></div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 col-sm-3 service-grid service6">
+					<div class="row services-item">
+						<img src="images/service-icon-6.png" alt="Manufactory">
+						<div class="services-info">
+							<h4><a href="#">IP Telephony</a></h4>
+							<div class="underline2"></div>
+							
+						</div>
+					</div>
+				</div>
+			
+
+			</div>
+
+		</div>
+	</div>
+	<!-- //Services -->
+
+
+	<!-- Clients -->
+	<div class="clients" id="clients">
+
+		<div class="container">
+
+			<h3><a href="#">OUR VALUABLE CLIENTS </a></h3>
+
+			<div class="underline1"></div>
+
+			<div class="logoslider clearfix">
+				<ul id="logoslider"> 
+					<li><i><img src="images/1.png"></i></li> 
+					<li><i><img src="images/2.png"></i></li> 
+					<li><i><img src="images/3.png"></i></li> 
+					<li><i><img src="images/4.png"></i></li> 
+					<li><i><img src="images/5.png"></i></li> 
+					<li><i><img src="images/6.png"></i></li> 
+					<li><i><img src="images/7.png"></i></li> 
+					<li><i><img src="images/8.png"></i></li> 
+					<li><i><img src="images/9.png"></i></li> 
+					<li><i><img src="images/10.png"></i></li> 
+					<li><i><img src="images/11.png"></i></li> 
+					<li><i><img src="images/12.png"></i></li> 
+					<li><i><img src="images/13.png"></i></li> 
+					<li><i><img src="images/14.png"></i></li>   
+					                                        
+				</ul>
+			</div>
+
+
+			
+
+		</div>
+	</div>
+	<!-- //Clients -->
+
+	<!-- Partners -->
+	<div class="partners" id="partners">
+
+		<div class="container">
+
+			<h3><a href="#">OUR ALLIANCES </a></h3>
+			<div class="underline1"></div>
+
+
+			<div class="row">
+				
+				<div class="col-md-2 col-sm-2 partner-logo">
+					<img src="images/g.png" alt="BR">
+				</div>
+				<div class="col-md-2 col-sm-2 partner-logo">
+					<img src="images/b.png" alt="BR">
+				</div>
+				<div class="col-md-2 col-sm-2 partner-logo">
+					<img src="images/c.png" alt="BR">
+				</div>
+				<div class="col-md-2 col-sm-2 partner-logo">
+					<img src="images/d.jpg" alt="BR">
+				</div>
+				<div class="col-md-2 col-sm-2 partner-logo">
+					<img src="images/e.png" alt="BR">
+				</div>
+				<div class="col-md-2 col-sm-2 partner-logo">
+					<img src="images/f.png" alt="BR">
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Partners -->
+
+	<!-- Contact -->
+	<div class="contact" id="contact">
+		<div class="container">
+
+			<h3>CONTACT</h3>
+			
+			<div class="underline1"></div>
+
+			<div class="contact-grids clearfix">
+
+				<div class="col-md-6 col-sm-6 contact-grid">
+					<form>
+						<input type="text" class="text" placeholder="Name" required="">
+						<input type="text" class="text" placeholder="Email" required="">
+						<textarea placeholder="Message" required=""></textarea>
+						<input type="submit" class="more_btn" value="Send">
+					</form>
+				</div>
+
+				<div class="col-md-6 col-sm-6 contact-grid">
+					<h4>CONTACT INFO</h4>					
+					<p style="font-weight:bold; font-size:18px;">NexGen IT Solutions Pvt. Ltd</p>
+					
+					<!-- <div class="contact-phone">
+						<p><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>Office Tel : 00977-1-4721121</p>
+						<p><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>Cell : 00977-9801025333</p>
+					</div> -->
+					<div class="contact-email">
+						<p><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Email : <a href="mailto:info@nexgen.com.np">info@nexgen.com.np</a></p> 
+						<p><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Website : <a href="http://nexgen.com.np/">www.nexgen.com.np</a></p>
+					</div>
+					<div class="address">
+						<h5>ADDRESS</h5>
+						<address>
+							<ul>
+								<li>Trade Tower,</li>
+								<li>Thapathali,</li>
+								<li>Kathmandu, Nepal</li>
+								
+							</ul>
+						</address>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+	<!-- //Contact -->
+
+
+	<!-- Footer  -->
+	<footer>
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-6 col-sm-12 right-border">
+					<div class="about-section clearfix">
+						<div class="footer-about">
+							<h2 class="footer-title">About Us</h2>
+							<p>NexGen IT Solution Pvt. LTD is a firm based in Kathmandu that is committed to provide End-to End IT solutions, with an aim to contribute to the IT infrastructure requirements of companies of all sizes in Nepal. We lay the ground work for electronic highways required for successful Information technology implementations and provide end to end hardware solutions to companies of all sizes.</p>
+						</div>
+
+						<div class="navigation left-border">
+							<h2 class="footer-title">Navigation</h2>
+							<ul class="list-unstyled">
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Alliances</a></li>
+								<li><a href="#">Partners</a></li>
+								<li><a href="#">Contact Us</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="copyright hidden-sm hidden-xs">
+						<p>Copyrights &copy; 2016 <a href="#" title="">NexGen</a>. All Right Reserved.</p>
+						<p>Designed & Developed By : <a href="#" title="">Arun Dhungel</a></p>
+					</div>
+
+				</div>
+
+				<div class="col-md-6 col-sm-12 clearfix">
+
+					<div class="clearfix">
+
+						<div class="col-md-5 col-sm-5">
+
+							<div class="contact-info">
+								<h2 class="footer-title">Contact Info</h2>
+								<div class="single">
+									<i class="fa fa-map-marker"></i>
+									<p>Trade Tower <br>
+										Thapathali, Kathmandu
+									</p>
+								</div>
+								<div class="single">
+									<i class="fa fa-phone"></i>
+									<p>00977-1-4721121</p>
+								</div>
+
+								<div class="single">
+									<i class="fa fa-envelope"></i>
+									<p>info@nexgen.com.np</p>
+								</div>
+
+								<div class="social-icon">
+									<ul class="list-inline">
+										<li><a href="http://sirseni.com/" title=""><i class="fa fa-facebook"></i></a></li>
+										<li><a href="http://sirseni.com/" title=""><i class="fa fa-twitter"></i></a></li>
+										<li><a href="http://sirseni.com/" title=""><i class="fa fa-instagram"></i></a></li>
+										<li><a href="http://sirseni.com/" title=""><i class="fa fa-linkedin"></i></a></li>
+									</ul>
+								</div>
+
+							</div>
+
+						</div>
+						<div class="col-md-7 col-sm-7  left-border">
+
+							<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+
+						</div>
+					</div>
+
+					<div class="copyright visible-sm visible-xs">
+						<p>Copyrights &copy; 2016 <a href="#" title="">NexGen</a>. All Right Reserved.</p>
+						<p>Developed By : <a href="#" title="">Arun Dhungel</a></p>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</footer>
+
+    <a href="#" id="back-to-top"></a>
+
+
+	<!-- //Footer -->
+
+	<!-- Custom-JavaScript-File-Links -->
+
+		<!-- Necessary-JavaScript-File-For-Bootstrap --> <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+		<!-- Slider-JavaScript -->
+			<script src="js/responsiveslides.min.js"></script>
+
+
+			<script>
+				$(function () {
+					$("#slider").responsiveSlides({
+						auto: true,
+						pager: true,
+						nav: true,
+						speed: 1000,
+						namespace: "callbacks",
+						before: function () {
+							$('.events').append("<li>before event fired.</li>");
+						},
+						after: function () {
+							$('.events').append("<li>after event fired.</li>");
+						}
+					});
+				});
+			</script>
+			
+		<!-- //Slider-JavaScript -->
+
+
+		<!-- Slide-To-Top JavaScript (No-Need-To-Change) -->
+			<script type="text/javascript">
+				$(document).ready(function() {
+					var defaults = {
+						containerID: 'toTop', // fading element id
+						containerHoverID: 'toTopHover', // fading element hover id
+						scrollSpeed: 100,
+						easingType: 'linear'
+					};
+					$().UItoTop({ easingType: 'easeOutQuart' });
+				});
+			</script>
+			<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 0;"> </span></a>
+		<!-- //Slide-To-Top JavaScript -->
+
+		<!-- Smooth-Scrolling-JavaScript -->
+			<script type="text/javascript" src="js/move-top.js"></script>
+			<script type="text/javascript" src="js/easing.js"></script>
+			<script type="text/javascript">
+					jQuery(document).ready(function($) {
+						$(".navbar li a").click(function(event){
+							$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+						});
+					});
+			</script>
+		<!-- //Smooth-Scrolling-JavaScript -->
+		
+		<!-- FlexSlider-JavaScript -->
+			<script defer src="js/jquery.flexslider.js"></script>
+			<script type="text/javascript">
+				$(function(){
+					SyntaxHighlighter.all();
+				});
+				$(window).load(function(){
+					$('.flexslider').flexslider({
+						animation: "slide",
+						start: function(slider){
+							$('body').removeClass('loading');
+						}
+					});
+				});
+			</script>
+		<!-- //FlexSlider-JavaScript -->
+
+	<!-- //Custom-JavaScript-File-Links -->
+	<script>
+
+		            $(document).on('click', '.menu__link', function (event) {
+	            		$('.menu__list').addClass('collapse');
+		            	$('.navbar-collapse').removeClass('collapse in');
+
+	        });
+
+		        $(document).on('click', '.navbar-toggle', function (event) {
+		            $('.menu__list').removeClass('collapse');
+
+		        });
+
+		        $(document).on('click', 'body', function () {
+                	$('.menu__list').addClass('collapse');
+            });
+		        $(document).on('click', 'nav ', function(event) {
+                event.stopImmediatePropagation();
+            });
+
+            
+	            
+	        </script>
+
+	        <script>
+        
+
+            $(document).on('click', 'nav li', function () {
+                $(this).siblings('li').removeClass('active');
+                $(this).addClass('active');        
+            });
+			
+           
+            
+        </script>
+
+        <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<script type="text/javascript">
+
+    $(window).load(function() {
+        $("#logoslider").flexisel({
+            visibleItems: 6,
+            itemsToScroll: 1,
+            animationSpeed: 200,
+            infinite: false,
+            navigationTargetSelector: null,
+            autoPlay: {
+                enable: false,
+                interval: 5000,
+                pauseOnHover: true
+            },
+            responsiveBreakpoints: { 
+                portrait: { 
+                    changePoint:480,
+                    visibleItems: 1,
+                    itemsToScroll: 1
+                }, 
+                landscape: { 
+                    changePoint:640,
+                    visibleItems: 2,
+                    itemsToScroll: 2
+                },
+                tablet: { 
+                    changePoint:768,
+                    visibleItems: 3,
+                    itemsToScroll: 3
+                }
+            }
+        });
+        
+        
+    });
+</script>
+
+
+
+   
+</body>
+<!-- //Body -->
+
+
+
+</html>
